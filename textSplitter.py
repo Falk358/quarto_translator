b77049ee5deb9b4234afbc4098dd3faab740797d
Falk358 <maxdanelli@gmail.com>
2023-11-13 11:17:33 +0100

    added method to textSplitter to iterate over all docs

--- textSplitter.py
+++ textSplitter.py
@@ -20,6 +20,20 @@ class textSplitter():
         ]
         self.md_text_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
 
+
+    def splitTextFileDict(self, file_dict: dict):
+        """
+        takes a dictionary with file path as key and markdown text as input;
+        iterates over dictionary and returns dict with filepath as key; langchain.schema.document.Document as value
+        """
+        split_doc_dict = {}
+        for key, document in file_dict.items():
+            split_doc = self.splitText(document)
+            split_doc_dict[key] = split_doc
+        
+        return split_doc
+
+
     def splitText(self, document_string):
         split_text = self.md_text_splitter.split_text(document_string)
         return split_text
